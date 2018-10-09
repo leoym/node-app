671bc739ffd46816c4542f3e34786ee179bf31ac
root <root@localhost.localdomain>
2018-10-09 18:48:06 +0000

    App files

--- app/routes/reports.js
+++ app/routes/reports.js
@@ -1,3 +1,7 @@
+const fetch = require('node-fetch');
+
+var fs = require('fs');
+
 var express = require('express');
 var router = express.Router();
 
@@ -22,6 +26,14 @@ router.get('/', function(req, res, next) {
   }, function(error) {
   console.trace(error.message);
   });
+});
+
+router.get('/abc', function(req, res, next) {
+    res.send(GetSite());
+});
+
+router.get('/xyz', function(req, res, next) {
+    res.send('{id:xyz}');
 });
 
 /* GET reports track listing. */
@@ -66,5 +78,69 @@ router.get('/add', function(req, res, next) {
 
   res.send(String(data.getTime()) + "-"+ rand);
 });
+
+router.get('/teste', (req, res) => {
+  //fetch('http://127.0.0.1/')
+  fetch('http://localhost:3000/demofile1.html')
+    .then(res => res.text())
+    .then(body => {
+    console.log(body);
+    console.log(body);
+    console.log(body);
+    console.log(body);
+    console.log(body);
+    res.send(body);
+  }).catch((error) => {
+    logger.error(error.message, error);
+    res.status(error.response.status).send(error.response.data);
+  });
+});
+
+router.get('/file', (req, res) => {
+  fs.readFile('demofile1.html', function(err, data) {
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.write(data);
+     res.end();
+  });
+});
+
+router.get('/file1', (req, res) => {
+  fs.readFile('demofile1.html', function(err, data) {
+   
+  var fs1 = require('fs');
+
+  //const random = require('random')
+  //arq = random.int(); 
+
+  var rn = require('random-number');
+  var options = {
+    min: 0
+    , max: 1000000
+    , integer: true
+  }
+
+  var arq = rn(options);
+
+    fs1.writeFile( arq + '-mynewfile3.txt', 'Hello content!', function (err) {
+      if (err) throw err;
+         console.log('Saved!');
+    });
+
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.write(data);
+    res.end();
+  });
+});
+
+function GetSite() {
+    var resposta = 'X';
+    fetch('http://localhost:3000/reports/xyz')
+    .then(res => res.text())
+    //.then(body => console.log(body));
+    //.then(body => res.send(body));
+    .then(body => resposta = body);
+
+    return resposta;
+}
 
 module.exports = router;
