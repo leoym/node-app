8c907eb5b913f9ee088ec3ef15318aa0a2093eaa
root <root@jeferson-server.jeferson.local>
2018-12-17 19:52:18 +0000

    Systemd files

--- systemd/app.js
+++ systemd/app.js
@@ -1,7 +1,32 @@
 require('systemd');
  
-var http = require('http');
-http.createServer(function (req, res) {
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end('Hello World!\n');
-}).listen('systemd');
+var express = require('express');
+var app = express();
+
+app.use(express.static('public'));
+
+app.set('views','views');
+app.set('view engine', 'pug');
+
+app.get('/', function (req, res) {
+  res.send('Hello World!');
+});
+
+app.get('/health', function (req, res) {
+  res.send('Status UP');
+});
+
+app.get('/teste', function (req, res) {
+  res.send('Teste UP');
+});
+
+app.get('/info', function (req, res) {
+  res.render('index', { title: 'Hey', message: 'Hello there!'});
+});
+
+app.use(function(req, res, next) {
+  res.status(404).send('Sorry cant find that!');
+});
+
+app.listen('systemd');
+
